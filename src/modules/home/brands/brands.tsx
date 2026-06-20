1108313135da44910a1e86fd30ea9996bd5806c5
Samim Middey <samim.middey@broadifitech.com>
2026-06-20 19:20:32 +0530

    fix: replace swiper with embla carousel

--- src/modules/home/brands/brands.tsx
+++ src/modules/home/brands/brands.tsx
@@ -1,50 +1,41 @@
 "use client";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper/modules';
-
-// Import Swiper styles
-import 'swiper/css';
+import useEmblaCarousel from 'embla-carousel-react';
+import AutoScroll from 'embla-carousel-auto-scroll';
 import Image from 'next/image';
 
 const Brands = () => {
+	const [emblaRef] = useEmblaCarousel(
+		{ loop: true, watchDrag: false },
+		[AutoScroll({ playOnInit: true, stopOnInteraction: false, stopOnMouseEnter: false, stopOnFocusIn: false, speed: 1 })]
+	);
+
 	return (
 		<div className='brands-cover'>
-			<Swiper
-				modules={[Autoplay]}
-				slidesPerView="auto"
-				spaceBetween={80}
-				loop
-				speed={25000}
-				autoplay={{
-					delay: 1,
-					disableOnInteraction: false,
-				}}
-				allowTouchMove={false}
-			>
-				{
-					Array.from({ length: 5 }).map((_, index) => (
-						<SwiperSlide key={index} style={{ width: 'auto' }}>
+			<div className="overflow-hidden" ref={emblaRef}>
+				<div className="flex">
+					{Array.from({ length: 5 }).map((_, index) => (
+						<div key={index} className="flex-none" style={{ width: 'auto', marginRight: '80px' }}>
 							<div className="flex items-center gap-10 md:gap-12 lg:gap-16 2xl:gap-20">
-									{Array.from({ length: 5 }).map((_, idx) => (
-										<div
-											key={idx}
-											className="relative w-20 md:w-24 h-10"
-										>
-											<Image
-												src={`/home/brands/img-${idx + 1}.png`}
-												alt={`Brand ${index + 1}`}
-												className="object-contain grayscale opacity-75"
-												fill
-												sizes="100%"
-											/>
-										</div>
-									))}
-								</div>
-						</SwiperSlide>
-					))
-				}
-			</Swiper>
+								{Array.from({ length: 5 }).map((_, idx) => (
+									<div
+										key={idx}
+										className="relative w-20 md:w-24 h-10"
+									>
+										<Image
+											src={`/home/brands/img-${idx + 1}.png`}
+											alt={`Brand ${index + 1}`}
+											className="object-contain grayscale opacity-75"
+											fill
+											sizes="100%"
+										/>
+									</div>
+								))}
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
 		</div>
 	);
 };
